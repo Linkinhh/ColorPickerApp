d906f6df20a99cfc76882f2ec0df0ce4b5fd12d6
Linkinhh <steban90starkk@gmail.com>
2024-09-04 00:35:22 -0300

    configuration_imgui

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// "" <-- local || <> extern
+#include <fmt/core.h>
+#include <iostream>
+#include <imgui.h>
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+int main()
+{
+    fmt::print("Hello World!\n");
+    std::cout << "asd";
+    return 0;
+}
